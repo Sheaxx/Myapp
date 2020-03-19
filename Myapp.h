a494564170fbd10eca47bc7b52a32a7304960f60
PIPIYing <19924338329@163.com>
2020-03-19 23:09:13 +0800

    更新分数的加法运算
    bug1：分子为0，打印0
    bug2：分母为0，排除此情况
    bug3：分母为1，打印分子
    bug4：分子与分母能整除，加判断
    bug5：化简分数，找出最大公因数

---
+++ Myapp.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+//生成运算符的函数
+char signal();
+//整数加减乘除的函数
+char arithmetic_integer(char sign, int r);
+//整数除法的函数
+char division_integer(int a, int b);
+//分数加减的函数
+char add_sub_fraction(char sign, int r);
+//假分数转化为带分数的函数
+void change(int a, int b, int* m, int* r);
+//辗转相除法求最大公因数
+int max_common(int m, int n);
+//辗转相除法求最小公倍数
+int min_common(int m, int n);
+//获取不为0的随机数
+int random(int r);
